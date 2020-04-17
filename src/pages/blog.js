9b08186b8c8dc6b56d8c054a8db5b3d44ed08acb
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-17 00:42:38 -0400

    chore: link to blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout'
-import { graphql, useStaticQuery } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 
 const BlogPage = () => {
@@ -32,12 +32,14 @@ const BlogPage = () => {
             <ol>
                 {posts.map(post =>
                     <li key={post.node.fields.slug}>
-                        <h2>{post.node.frontmatter.title}</h2>
-                        <p>{post.node.frontmatter.date}</p>
+                        <Link to={`/blog/${post.node.fields.slug}`}>
+                            <h2>{post.node.frontmatter.title}</h2>
+                            <p>{post.node.frontmatter.date}</p>
+                        </Link>
                     </li>
                 )}
             </ol>
-        </Layout>
+        </Layout >
     )
 }
 
---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            Blog Template
+        </Layout>
+    )
+}
+
+export default Blog
