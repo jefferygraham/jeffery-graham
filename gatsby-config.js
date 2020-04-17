c239460416725b634c25bab157594f241a7d6d69
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-17 15:16:14 -0400

    chore: add/configure contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -36,6 +40,13 @@ module.exports = {
           }
         ],
       },
-    }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ]
 }
