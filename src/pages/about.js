cf9f324b58ad74a528ef7140ba2430c2a955d141
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-17 21:23:13 -0400

    chore: add hemet

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const AboutPage = () => {
     return (
         <Layout>
+            <Head title="About" />
             <h1>About</h1>
             <p>Bio goes here...</p>
             <p>Need a developer? <Link to='/contact'>Contact me.</Link></p>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout'
 import { Link, graphql, useStaticQuery } from 'gatsby'
+import Head from '../components/head'
 
 import blogStyles from './blog.module.scss'
 
 
 const BlogPage = () => {
 
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
         allContentfulBlogPost (
           sort: {
@@ -27,23 +28,24 @@ const BlogPage = () => {
       }
     `)
 
-    const posts = data.allContentfulBlogPost.edges;
+  const posts = data.allContentfulBlogPost.edges;
 
-    return (
-        <Layout>
-            <h1>Blog</h1>
-            <ol className={blogStyles.posts}>
-                {posts.map(post =>
-                    <li key={post.node.slug} className={blogStyles.post}>
-                        <Link to={`/blog/${post.node.slug}`}>
-                            <h2>{post.node.title}</h2>
-                            <p>{post.node.publishedDate}</p>
-                        </Link>
-                    </li>
-                )}
-            </ol>
-        </Layout >
-    )
+  return (
+    <Layout>
+      <Head title="Blog" />
+      <h1>Blog</h1>
+      <ol className={blogStyles.posts}>
+        {posts.map(post =>
+          <li key={post.node.slug} className={blogStyles.post}>
+            <Link to={`/blog/${post.node.slug}`}>
+              <h2>{post.node.title}</h2>
+              <p>{post.node.publishedDate}</p>
+            </Link>
+          </li>
+        )}
+      </ol>
+    </Layout >
+  )
 }
 
 export default BlogPage;
